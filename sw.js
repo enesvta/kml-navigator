6a3d0f84aa2d653365e388aca511b18441edba33
enesvta <129265803+enesvta@users.noreply.github.com>
2026-03-04 19:16:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kml-nav-v3";
+const CACHE = "kml-nav-v4";
 
 // Base path’i otomatik bulur: örn "/kml-navigator/"
 const BASE = new URL("./", self.location).pathname.replace(/\/$/, "");
@@ -32,3 +32,4 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(`${BASE}/index.html`)))
   );
 });
+
