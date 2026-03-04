3d645d1b58afd3c0a42f0d976de51066f94507b3
enesvta <129265803+enesvta@users.noreply.github.com>
2026-03-04 19:15:09 +0300

    Upgrade service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE = "kml-nav-v2";
+const CACHE = "kml-nav-v3";
 
-// GitHub Pages’de /repo-adi/ altında çalışır.
-// Base path’i otomatik bul: örn "/kml-navigator/"
+// Base path’i otomatik bulur: örn "/kml-navigator/"
 const BASE = new URL("./", self.location).pathname.replace(/\/$/, "");
+
 const ASSETS = [
   `${BASE}/`,
   `${BASE}/index.html`,
@@ -28,8 +28,7 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  const req = e.request;
   e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).catch(() => caches.match(`${BASE}/index.html`)))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(`${BASE}/index.html`)))
   );
 });
