11a0d13c2f98b46e26095a5c95c192a968b208a1
enesvta <129265803+enesvta@users.noreply.github.com>
2026-03-09 18:26:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,6 @@
-const CACHE = "central-saha-v51";
+self.addEventListener("install",e=>{
+self.skipWaiting()
+})
 
-const BASE = new URL("./", self.location).pathname.replace(/\/$/, "");
-const ASSETS = [
-  `${BASE}/`,
-  `${BASE}/index.html`,
-  `${BASE}/style.css`,
-  `${BASE}/app.js`,
-  `${BASE}/manifest.webmanifest`,
-  `${BASE}/sw.js`,
-  `${BASE}/icons/logo.jpg`,
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(`${BASE}/index.html`)))
-  );
-});
-
+self.addEventListener("fetch",e=>{
+})
